5731962222b7a458c0c9bb8b68a88f6e93d0d42e
lutzzdias <thiagolutzdias@hotmail.com>
2024-11-24 22:18:12 -0400

    docs: add swagger

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import books from "./book/router.js";
+import yaml from "yamljs";
+
 const app = express();
+const swaggerDoc = yaml.load("swagger.yaml");
+
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use(express.json());
 
