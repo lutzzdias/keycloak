5be97a9b43cfe4c14fa8af0ff7f7e60dadcbc3c8
lutzzdias <thiagolutzdias@hotmail.com>
2024-12-01 20:45:59 -0400

    feat: add keycloack verification/validation
    not tested due to token issuer website being down

--- server.js
+++ server.js
@@ -2,20 +2,25 @@ import express from "express";
 import swaggerUi from "swagger-ui-express";
 import books from "./book/router.js";
 import yaml from "yamljs";
+import Keycloak from "keycloak-connect";
 
 const app = express();
 const swaggerDoc = yaml.load("swagger.yaml");
+const keycloak = new Keycloak(
+  {},
+  {
+    serverUrl: "https://tdsoft-auth.hsborges.dev",
+    bearerOnly: true,
+    realm: "trabalho-pratico",
+  },
+);
 
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use(express.json());
+app.use(keycloak.middleware());
 
-const authenticate = (req, _, next) => {
-  console.log(req.headers.authorization);
-  next();
-};
-
-app.use("/books", authenticate, books);
+app.use("/books", keycloak.protect(), books);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
